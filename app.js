1fbb31cddbf11d7818b07c254262f0fe21386fa5
AntonellaMilla <antonella.milla@tecsup.edu.pe>
2025-09-13 10:12:32 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// Configurar el motor de vistas
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Servir archivos estáticos desde "public"
+app.use(express.static(path.join(__dirname, "public")));
+
+// Importar rutas
+const mainRoutes = require("./routes/mainRoutes");
+app.use("/", mainRoutes);
+
+// Iniciar el servidor
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
+
+
+
+
+
+
+
+// Para procesar datos de formularios
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+
+
+
+
+
+
+// Middleware 404
+app.use((req, res, next) => {
+  res.status(404).render("notFound", { url: req.originalUrl });
+});
